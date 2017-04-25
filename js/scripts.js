143a145d63bd31faaad5c8925f92b30034902ab7
Corey Redding and Rose Sponseller <corey@>
2017-04-25 16:11:09 -0700

    Add several lines of jQuery to get text to toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
-  $("#text-button").click(function() {
-    $(this).toggle();
-    });
+  $("h3").click(function) {
+    $(".description-hidden").toggle();
+    $(".description-showing").toggle();
   });
+});
+
+// $(function() {
+// $(".buttons").click(function() {
+//   $(this).children(".buttons:nth-child(3)").toggle();
+//   });
+// });
+
+
+
+  // $(".btn").click(function() {
+  //   $(this).toggle(.children());
+  //   $(".description-shown").toggle();
+  //   });
+  // });
 //   $(".btn").click(function() {
 //     $(this).toggle();
 //   });
